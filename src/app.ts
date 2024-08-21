4d8271ad437ca24b1235447a199dd92fbb39a133
cyberkaidev <cyberkaidev@gmail.com>
2024-08-21 20:14:17 -0300

    :wrench: added middleware error and new routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import express from "express";
+import "express-async-errors";
+
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import logger from "morgan";
 
-import { router } from "./routes";
+import { AppError } from "./errors/app-error";
+import { errorConstans } from "./constans/error-constans";
+import { bitcoinRoutesV1 } from "./api/v1/bitcoin-routes";
+import { deprecated } from "./api/deprecated/bitcoin-routes";
+
+const { internal_error } = errorConstans;
 
 /**
  * Created app
@@ -12,11 +19,28 @@ export const app = express();
 /**
  * Middleware settings
  */
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(logger("dev"));
 
 /**
  * Integrate endpoint at application
  */
-app.use("/", router);
+app.use("/v1/bitcoin", bitcoinRoutesV1);
+
+app.use("/v1", deprecated);
+
+/**
+ * Middleware error
+ */
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({ message: error.message });
+    }
+
+    return response
+      .status(internal_error.statusCode)
+      .json({ message: internal_error.message });
+  },
+);

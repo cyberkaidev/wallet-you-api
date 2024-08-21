b0b7df1a179c25c1fc14d9754d426c9fdd2c826a
cyberkaidev <cyberkaidev@gmail.com>
2024-08-21 20:13:09 -0300

    :fire: deleted files from the old structure

--- src/constans/error-structure-constans.ts
+++
@@ -1,5 +0,0 @@
-export const errorStructureConstans = {
-  empty_key: { status: 400, code: "EMPTY_KEY" },
-  key_not_found: { status: 404, code: "KEY_NOT_FOUND" },
-  internal_error: { status: 500, code: "INTERNAL_ERROR" },
-};

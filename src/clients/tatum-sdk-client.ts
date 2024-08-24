59a04323bee2c07e58c423ba7c3eec28c0dca48d
cyberkaidev <cyberkaidev@gmail.com>
2024-08-24 20:15:29 -0300

    :sparkles: created new function to access the ethereum network

--- src/clients/tatum-sdk-client.ts
+++ src/clients/tatum-sdk-client.ts
@@ -1,10 +1,19 @@
 import "dotenv/config";
-import { TatumSDK, Network, Bitcoin } from "@tatumio/tatum";
+import { TatumSDK, Network, Bitcoin, Ethereum } from "@tatumio/tatum";
 
 export class TatumSDKClient {
-  async init() {
+  async initBitcoin() {
     return await TatumSDK.init<Bitcoin>({
       network: Network.BITCOIN,
+      apiKey: {
+        v4: process.env.TATUM_SDK_V4,
+      },
+    });
+  }
+
+  async initStablecoin() {
+    return await TatumSDK.init<Ethereum>({
+      network: Network.ETHEREUM,
       apiKey: {
         v4: process.env.TATUM_SDK_V4,
       },

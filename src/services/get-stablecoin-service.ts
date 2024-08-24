1ae61ea38352d3a7a30e58ffdb2f3633f4bb40ec
cyberkaidev <cyberkaidev@gmail.com>
2024-08-24 20:26:08 -0300

    :sparkles: class created to get balance and transaction from tatum sdk

---
+++ src/services/get-stablecoin-service.ts
@@ -0,0 +1,37 @@
+import { TatumSDKClient } from "../clients/tatum-sdk-client";
+import { tokenAddresses } from "../constans/tokens-addresses-constans";
+import { StablecoinSDKConfig } from "../interfaces/get-stablecoin-interface";
+
+export class GetStablecoinService {
+  async getBalanceFromSDK(key: string) {
+    const { initStablecoin } = new TatumSDKClient();
+
+    const { address } = await initStablecoin();
+
+    return await address.getBalance({
+      addresses: [key],
+      tokenTypes: ["fungible"],
+      pageSize: 50,
+    });
+  }
+
+  async getTransactionsFromSDK(key: string) {
+    const { initStablecoin } = new TatumSDKClient();
+    const { USDC_ETH, USDT_ETH } = tokenAddresses;
+
+    const config: StablecoinSDKConfig = {
+      address: key,
+      transactionTypes: ["fungible"],
+      pageSize: 50,
+    };
+
+    const { address } = await initStablecoin();
+
+    const [usdc, usdt] = await Promise.all([
+      address.getTransactions({ ...config, tokenAddress: USDC_ETH }),
+      address.getTransactions({ ...config, tokenAddress: USDT_ETH }),
+    ]);
+
+    return { usdc, usdt };
+  }
+}

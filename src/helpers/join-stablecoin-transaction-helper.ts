1f171ca3af45ab7843559e25b4aa8a3905c1db17
cyberkaidev <cyberkaidev@gmail.com>
2024-08-24 20:23:36 -0300

    :sparkles: function created to join transactions and order by date

---
+++ src/helpers/join-stablecoin-transaction-helper.ts
@@ -0,0 +1,11 @@
+import {
+  JoinStablecoinTransaction,
+  JoinStablecoinTransactionProps,
+} from "../interfaces/join-stablecoin-transaction-interface";
+
+export function joinStablecoinTransaction({
+  usdc,
+  usdt,
+}: JoinStablecoinTransactionProps): JoinStablecoinTransaction[] {
+  return [...usdc, ...usdt].sort((x, y) => x.timestamp - y.timestamp).reverse();
+}

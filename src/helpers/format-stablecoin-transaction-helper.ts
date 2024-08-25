94317c6545360e1c4d01c04b8e1651269783a261
cyberkaidev <cyberkaidev@gmail.com>
2024-08-25 18:19:17 -0300

    :hammer: reduced code to isolate and separate responsibilities

--- src/helpers/format-stablecoin-transaction-helper.ts
+++ src/helpers/format-stablecoin-transaction-helper.ts
@@ -1,38 +1,29 @@
-import moment from "moment";
-import { tokenAddresses } from "../constans/tokens-addresses-constans";
 import {
   FormatStablecoinTransactionHelper,
   Grouped,
-  TransactionItem,
 } from "../interfaces/format-stablecoin-transaction-interface";
 import { JoinStablecoinTransaction } from "../interfaces/join-stablecoin-transaction-interface";
+import { filterAndFormatTransactions } from "./filter-and-format-transactions";
 
 export function formatStablecoinTransactionHelper(
   data: JoinStablecoinTransaction[],
 ): FormatStablecoinTransactionHelper[] {
-  const { USDC_ETH } = tokenAddresses;
-
   const transactions: FormatStablecoinTransactionHelper[] = [];
 
-  const formatted: TransactionItem[] = data.map(item => ({
-    tokenId: item.tokenId,
-    amount: item.amount,
-    transactionSubtype: item.transactionSubtype,
-    tokenName: item.tokenAddress === USDC_ETH ? "USDC" : "USDT",
-    timestamp: item.timestamp,
-    hours: moment(item.timestamp).format("LT"),
-    date: moment(item.timestamp).format("L"),
-  }));
+  const { listFormatted } = filterAndFormatTransactions(data);
 
-  const grouped: Grouped = formatted.reduce((accumulator: Grouped, item) => {
-    const date = item.date;
+  const grouped: Grouped = listFormatted.reduce(
+    (accumulator: Grouped, item) => {
+      const date = item.date;
 
-    if (!accumulator[date]) accumulator[date] = [];
+      if (!accumulator[date]) accumulator[date] = [];
 
-    accumulator[date].push({ ...item });
+      accumulator[date].push({ ...item });
 
-    return accumulator;
-  }, {});
+      return accumulator;
+    },
+    {},
+  );
 
   Object.keys(grouped).forEach(item => {
     transactions.push({
